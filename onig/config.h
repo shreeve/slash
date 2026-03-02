74ab904e8cadd79bbb8450df576848466f8b1ed5
Steve Shreeve <steve.shreeve@gmail.com>
2026-03-02 06:39:00 -0700

    Add onig

---
+++ onig/config.h
@@ -0,0 +1,28 @@
+/* Oniguruma config.h for Zig/POSIX builds */
+
+#include <stdio.h>
+
+#define HAVE_ALLOCA 1
+#define HAVE_ALLOCA_H 1
+#define HAVE_DLFCN_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TIMES_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define STDC_HEADERS 1
+
+#define SIZEOF_INT 4
+#define SIZEOF_LONG 8
+#define SIZEOF_LONG_LONG 8
+#define SIZEOF_VOIDP 8
+
+#define PACKAGE "oniguruma"
+#define PACKAGE_VERSION "6.9.9"
+#define VERSION "6.9.9"
